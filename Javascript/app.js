293d3722823083512e71f16658e13edfa4acd5e0
MuhammadArsalanNaeem2004 <130148088+MuhammadArsalanNaeem2004@users.noreply.github.com>
2023-05-17 17:23:38 +0500

    Add files via upload

---
+++ Javascript/app.js
@@ -0,0 +1,568 @@
+// Chapter No 01
+
+// question 1
+
+
+// var a ;
+// a = "First Name";
+// alert(a)
+
+// var a ;
+// a = "Last Name";
+// alert(a)
+
+// var a ;
+// a = "Email";
+// alert(a)
+
+// var a ;
+// a = "Phone Number";
+// alert(a)
+
+// var a ;
+// a = "Password";
+// alert(a)
+
+
+// Question 2
+
+
+// var a ;
+// a = "You're learning JavaScript!";
+// alert(a)
+
+
+// Question 3
+
+
+// var a ;
+// a = "End Of Chapter No 01";
+// alert(a)
+
+
+// Chapter No 02
+
+// Question 1
+
+
+// var royalMen ;
+// var royalMen  = "Java Script";
+// alert(royalMen)
+
+
+// Question 2
+
+
+// var a ;
+// var a  = "Muhammad Arsalan";
+// alert(a)
+
+
+// Question 3
+
+
+// var teamName ;
+// var teamName = "React Native";
+// alert(teamName)
+
+
+// Question 4
+
+
+// var bestMan = "Charlie";
+// bestMan = "bestMan";
+// alert(bestMan)
+
+
+
+// Chapter No 3
+
+
+// Question 1,2
+
+// var caseQty ;
+// caseQty = 144 ;
+// alert(caseQty)
+
+
+// Question 3
+
+// var num ;
+// num = 9 ;
+
+
+// Question 4
+
+// var a = 2 ;
+// var b = 5 ;
+// var c = a+b ;
+// alert(c)
+
+
+// Question 5
+
+// var shirtTotal = 500 ;
+// var shipping = 100 ;
+// var Ordertotal = shirtTotal + shipping ;
+// alert(Ordertotal)
+
+
+// Question 6
+
+// var a = 2 ;
+// a = 5 ;
+// var b = 5 ;
+// var c = a+b ;
+// alert(c)
+
+
+// Chapter No 4
+
+
+// Question 1
+
+
+// var productcost = 3.45;
+// alert(productcost)
+
+
+// Question 2
+
+// var nameofBand;
+
+
+// Question 4
+
+// var arsalanNaeem;
+
+
+// Question 5
+
+// Legal Variables
+
+// var myVariable;
+// var _privateVariable;
+// var $specialVariable;
+// var camelCase;
+// var number1;
+
+// Illegal Variables
+
+// var 123number; 
+// var special@var; 
+// var #illegalVar;
+// var if; 
+// var class; 
+
+
+// Chapter No 5
+
+
+// Question 1
+
+// Remainder /Modulo 
+// it is represented by %
+
+
+// Question 2
+
+// var a = 20 ;
+// var b = 6 ;
+// var c = a%b ;
+// alert(c)
+
+
+// Question 3
+
+// var a = 1000*2000 ;
+// alert(a)
+
+
+// Question 4
+
+// var a = 5 ;
+// var b = 5 ;
+// var c = a-b ;
+// alert(c)
+
+
+// Question 5
+
+// var a = 15;
+// var b = 3;
+// var c = a % b;
+// console.log(c)
+
+
+// Question 6
+
+// alert(5 * 2);
+
+
+// Chapter No 6
+// Question 1
+
+// The Answer Is X++
+
+
+// Question 2
+
+// var a = 100;
+// var b = --a;
+//  alert(b)
+
+
+// Question 3
+
+// the answer is 50
+
+// var x = 50;
+// var y = x++;
+// alert(y)
+
+
+
+// Question 4
+
+// the answer is 49
+
+// var y = 50;
+// var z = --y;
+// alert(z)
+
+
+// Question 5
+
+// var a = 10;
+// var newNum = a--;
+
+
+// Question 6
+
+// var a = 10;
+// var b = a++;
+
+
+// Question 7
+
+// var number = 4;
+// number++;
+// alert("The new value is: " + number);
+
+
+// Chapter No 7
+
+// Question 1
+
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum); 
+// Answer: 14
+
+
+// Question 2
+
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum);
+ // Answer: 24
+
+
+//  Question 3
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+ // Answer: 24
+
+
+// Question 4
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum); 
+// Answer: 18
+
+
+
+// Question 8
+
+// Question 1
+
+// var num = "2" + "2";
+// console.log(num) 
+//Answer 22
+
+
+// Question 2
+
+// var message = ("Hello," + "Dolly");
+// alert(message)
+//Answer is Hello,Dolly
+
+
+// Question 3
+
+// alert("33" + 3);
+//Answer is 333
+
+
+// Question 4
+
+// var a = "Pakistan" + "Zindabad"
+// alert(a)
+ //Answer is PakistanZindabad
+
+
+
+
+// Chapter No 9
+
+// Question 1
+
+// prompt("Enter First Name")
+
+
+// Question 2
+// prompt("Country?","China")
+
+
+// Question 3
+
+// var yourName = prompt("Enter Your Name");
+
+
+// Question 5
+
+// var a1 = "Message";
+// var a3 = "Default response";
+// var b = prompt(a1, a3);
+
+
+// Question 6
+
+// var a = prompt("");
+// alert(a)
+
+
+
+// Chapter No 10
+
+// Question 1
+
+// var city = "Karachi"
+// if (city == "Karachi") {
+// console.log("The City OF Lights")}
+
+
+// Question 2
+
+// var x = 40;
+// var y = x;
+// if (x === y) {
+//     var z = prompt("Please enter the value of z:");
+//     }
+
+
+// Question 3
+
+// var ZipCode = "10010";
+// if (ZipCode === "10010") {
+//     alert("Karachi");
+//   } else {
+//     alert("Please write correct city");
+//   }
+
+// Question 4
+
+// x = 1;
+// if (x == 1) {
+//     x = 2; 
+//   }
+//   console.log(x)
+
+
+// Chapter No 11
+
+// Question 1
+
+// if (variable1 !== variable2) {
+//     // It can be Executed if the condition is true
+//   }
+
+
+// Question 2
+
+// if (variable1 >= variable2) {}
+
+
+// Question 3
+
+// if (variable !== 50) {
+//     variable = 25; 
+//   }
+
+
+// Chapter 12
+
+// Question 1
+
+// if (a <= 25) {
+//     alert("if statment")
+// } 
+// else if(a <= 70)
+// alert("else if statment")
+// else {
+//     alert("else statment")
+// }
+
+
+// Question 2
+
+// var marks = +prompt("Enter your marks:");
+
+
+// var percentage = (marks / 100) * 100;
+
+
+// var grade;
+
+// if (percentage >= 90) {
+//   grade = "A+";
+// } else if (percentage >= 80) {
+//   grade = "A";
+// } else if (percentage >= 70) {
+//   grade = "B";
+// } else if (percentage >= 60) {
+//   grade = "C";
+// } else {
+//   grade = "D";
+// }
+
+// console.log("Percentage: " + percentage + "%");
+// console.log("Grade: " + grade);
+
+
+// Question 3
+
+// if (a === 10) {
+//         alert("a is 10");
+//         }
+//         else{
+//             alert("The correct value of a is 10");
+//         }
+    
+
+// Chapter No 13
+
+// Question 1
+
+
+// var a = 12;
+// var b = 12;
+// var c = 22;
+// var d = 22;
+
+// if (a === b && c === d) {
+//    alert("True")
+//   }
+
+//   else{
+//     alert("False")
+//   }
+
+
+
+// Question 2
+
+
+// var a = 18;
+// var b = 125;
+// var c = 25;
+// var d = 255;
+
+// if (a === b || c !== d) {
+//    alert("True")
+//   }
+
+//   else{
+//     alert("False")
+//   }
+
+
+// Question 3
+
+
+// var name = prompt("Enter Name")
+
+// if (name === "Hamza" || name === "Arsalan") {
+//   alert("welcome "+ name);
+// }
+// var age = +prompt("Enter Age")
+
+// if (age < 60) {
+//   alert("Your Age is "+ age)
+// }
+
+    
+
+// Chapter No 14
+
+// Question 1
+
+
+// if (password !== "") {
+//   if (password.length <= 5) {
+//     alert("Password must be greater than 5.");
+//   } else {
+//     alert("OK");
+//   }
+// }
+
+// Question 2
+
+
+// var a = 1;
+
+// if (a === 1) {
+//   var c = "Max";
+//   if (c === "Max") {
+//   alert("OK");
+//   }
+//   }
+
+
+// Question 4
+
+
+// var a1 = 5;
+// var a2 = 5;
+
+// if (a1 === a2) {
+//   if (a1 <= a2) {
+//     alert("The conditions passed!");
+//   }
+// }
+
+
+// Chapter No 15
+
+// Question 1
+
+// var a = [];
+
+
+// Question 2
+
+// var myArray = ["Hello"];
+
+
+// Question 3
+
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]); 
+// Answer: "j"
+
+// Question 4
+
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var length = alphabet.length;
+// alert(length);
+// Answer: 8
